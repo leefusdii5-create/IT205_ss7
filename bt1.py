0a7291f8b459cd60a37b0af34f3c55f2475f21f5
DuyXinkGai <leefusdii5@gmail.com>
2026-05-29 08:11:45 +0700

    Update bt1.py

--- bt1.py
+++ bt1.py
@@ -1,12 +1,11 @@
-print("—— PHẦN MỀM TÍNH TỔNG QUỸ LƯƠNG ——")
+student_name = "  nguYEn vAn a  "
+student_code = "  rk-001-python  "
+email = "  Student01@GMAIL.COM  "
 
-total_budget = 0
+student_name = student_name.strip().title()
+student_code = student_code.strip().upper()
+email = email.strip().lower()
 
-for employee_number in range(1, 4):
-    print("Đang xử lý nhân viên số", employee_number)
-    salary = int(input("  Nhập mức lương (VNĐ): "))
-    total_budget = total_budget + salary
-
-print("= KẾT QUẢ: TỔNG NGÂN SÁCH CẦN CHUẨN BỊ LÀ:", total_budget, "VNĐ")
-
-# lối đặt total_budget ở trong vòng lập dẫn đến reset về 0 mỗi khi lập
+print("Họ tên:", student_name)
+print("Mã học viên:", student_code)
+print("Email:", email)
